bd3cc8350d6ba775cfda7fb3c0801c26a73bb633
Jay88 <jaeminp2@uci.edu>
2025-04-12 18:42:13 -0700

    reset

--- models/models.py
+++
@@ -1,6 +0,0 @@
-from models import db
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
-    is_done = db.Column(db.Boolean, default=False)
